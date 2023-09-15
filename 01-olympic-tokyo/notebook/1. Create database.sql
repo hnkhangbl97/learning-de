88648431a54eebce2793276a302075fdbafe6da3
Khang Nguyen <khangnguyen@Khangs-MacBook-Air.local>
2023-09-15 20:12:26 +0700

    Add complete project data

---
+++ 01-olympic-tokyo/notebook/1. Create database.sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+DROP DATABASE IF EXISTS olympic_tokyo_processed CASCADE;
+
+-- COMMAND ----------
+
+CREATE DATABASE IF NOT EXISTS olympic_tokyo_processed
+
+-- COMMAND ----------
+
+DROP DATABASE IF EXISTS olympic_tokyo_presentation CASCADE;
+
+-- COMMAND ----------
+
+CREATE DATABASE IF NOT EXISTS olympic_tokyo_presentation
+
+-- COMMAND ----------
+
+
